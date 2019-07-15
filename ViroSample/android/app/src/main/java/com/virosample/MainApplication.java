853bb5a4a9ba6d987ac90cff98f4727da67d12e5
Tuskemx <ma.edwards@live.com>
2019-07-15 10:44:01 +0100

    added map with user location  -- npm install @react-native-community/geolocation --save && react-native link @react-native community/geolocation && added uses permission to androidmanifest

--- ViroSample/android/app/src/main/java/com/virosample/MainApplication.java
+++ ViroSample/android/app/src/main/java/com/virosample/MainApplication.java
@@ -3,6 +3,7 @@ package com.virosample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new GeolocationPackage(),
           new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf(BuildConfig.VR_PLATFORM)), new MapsPackage());
     }
 
--- ViroSample/components/Maps.js
+++ ViroSample/components/Maps.js
@@ -1,12 +1,19 @@
 import MapView from 'react-native-maps';
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import Geolocation from '@react-native-community/geolocation'
+import React, { Component, Fragment } from 'react';
+import { Text, View, StyleSheet, Button, TouchableHighlight, Vibration } from 'react-native';
+
+
+
 
 // const Maps = () => {
 
 export default class Maps extends Component {
   state = {
-    color: '#FF0000'
+    color: '#FF0000',
+    latitude: 'change',
+    longitude: 'chagne',
+    errr: ''
   };
 
   handlePress = () => {
@@ -16,32 +23,60 @@ export default class Maps extends Component {
     });
   };
 
+  componentDidMount() {
+
+
+    Vibration.vibrate();
+    Geolocation.getCurrentPosition((info) => {
+      this.setState({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+
+      });
+    });
+    setTimeout(() => {
+      console.warn(this.state.latitude)
+      console.warn("hhello")
+    }, 5 * 1000)
+
+
+  }
+
   render() {
     // let { func } = this.props;
+    const location = {
+      latitude: 53.480759, longitude: -2.242631, title: "place", subtitle: "subplace"
+    }
+    const you = { longitude: this.state.longitude, latitude: this.state.latitude, title: "you", subtitle: "you" }
     return (
-      <>
-        <View style={styles.container}>
-          <MapView
-            style={styles.map}
-            region={{
-              latitude: 53.480759,
-              longitude: -2.242631,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121
-            }}
-          />
-        </View>
-        <View>
-          <Button
-            // onPress={this.handlePress}
-            onPress={this.props.getExperienceButtonOnPress}
-            title="Back"
-            color={this.state.color}
-            accessibilityLabel="Navigate back a page"
-          />
-          <Text>Maps</Text>
-        </View>
-      </>
+
+      <Fragment>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 53.480759,
+            longitude: -2.242631,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
+
+        >
+          <MapView.Marker coordinate={location} />
+          <MapView.Marker coordinate={you} />
+
+        </MapView>
+
+
+        <Button
+          // onPress={this.handlePress}
+          onPress={this.props.getExperienceButtonOnPress}
+          title="Back"
+          color={this.state.color}
+          accessibilityLabel="Navigate back a page"
+        />
+        <Text>{this.state.latitude} {this.state.longitude}</Text>
+      </Fragment>
+
     );
   }
 }
@@ -59,5 +94,6 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject
   }
 });
+
 
 // export default Maps;
